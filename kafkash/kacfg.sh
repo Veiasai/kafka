4b17ca4ec646690007be6904c384022748f0211e
veia-mac <917971502@qq.com>
2018-10-28 12:43:48 +0800

    10g-kafka-config

--- kafkash/kacfg.sh
+++ kafkash/kacfg.sh
@@ -1,8 +1,10 @@
 dir=kafka_2.12-2.0.0
 cfg=server.properties
+start=kafka-server-start.sh
 for i in 5 6 7;do
 id=slave-${i}
 rsync -avz --progress ./$cfg $id:/root/$dir/config/
+rsync -avz --progress ./$start $id:/root/$dir/bin/
 ssh $id << eeooff
 cd /root/$dir/config
 sed -i 's/broker.id=0/broker.id=$[$i-5]/g' $cfg
